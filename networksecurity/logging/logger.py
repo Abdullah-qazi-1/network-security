3a2c642b42181476d6ea75a646c99d957e490831
Abdullah-qazi-1 <abdullahizaq321@gmail.com>
2026-03-23 15:09:58 +0500

    Logging and Exception file made

---
+++ networksecurity/logging/logger.py
@@ -0,0 +1,18 @@
+import logging
+import os
+from datetime import datetime 
+
+LOG_FILE = f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
+
+# Correct logs directory
+logs_dir = os.path.join(os.getcwd(), "logs")
+os.makedirs(logs_dir, exist_ok=True)
+
+# Correct full file path
+LOG_FILE_PATH = os.path.join(logs_dir, LOG_FILE)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format='[%(asctime)s] %(levelname)s - %(message)s',
+    level=logging.INFO,
+)
